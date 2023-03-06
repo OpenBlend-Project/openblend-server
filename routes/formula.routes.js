bd4bd3591304bcaa609942d1b1e5e4b0ea6e8baf
binghatch <aron.seiffert@gmail.com>
2023-03-06 13:32:07 +0100

    feat: add get route for single formula

--- routes/formula.routes.js
+++ routes/formula.routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 // Error handling
 const errors = require('../utils/errors.utils');
 
 // Models
 const Formula = require('../models/Formula.model');
+
+// Middleware
+const { isAuthenticated } = require('../middleware/jwt.middleware');
 
 
 // POST /api/formulas - Creates a new formula
@@ -41,6 +45,64 @@ router.post("/formulas", (req, res, next) => {
   })
   .then((response) => res.json(response))
   .catch((error) => res.json(error));
+})
+
+
+
+// GET /api/formulas/:id - get formula with specific id that is associated to current user
+router.get("/formulas/:id", isAuthenticated, (req, res, next) => {
+  const formulaId = req.params['id'];
+  console.log(formulaId);
+  const userId = req.payload._id;
+  
+  Formula.findOne({ _id: formulaId, creator: userId })
+  .then((formulaFromDB) => {
+    const promises = formulaFromDB.ingredients.map(ingredient => {
+      // First, fetch the material for the current ingredient
+      return axios.get(`http://localhost:3005/api/materials/${ingredient.materialId}`)
+        .then((rawMaterialFromAPI) => {
+          // Add the material to the ingredient object
+          ingredient.material = rawMaterialFromAPI.data;
+
+          // Check if the ingredient has a solvent defined
+          if (ingredient.dilution && ingredient.dilution.solventId) {
+            // If so, fetch the solvent for the ingredient
+            return axios.get(`http://localhost:3005/api/materials/${ingredient.dilution.solventId}`)
+              .then((rawSolventFromAPI) => {
+                // Add the solvent to the dilution object
+                ingredient.dilution.solvent = rawSolventFromAPI.data;
+
+                // Return the modified ingredient object
+                return ingredient;
+              })
+              .catch((error) => res.json(error));
+          } else {
+            // If the ingredient has no solvent defined, return it as is
+            return ingredient;
+          }
+        })
+        .catch((error) => res.json(error));
+    });
+    return Promise.all(promises)
+      .then((ingredients) => {
+        // Replace the original ingredients array with the modified one
+        formulaFromDB.ingredients = ingredients;
+        return formulaFromDB;
+      });
+  })
+  .then((formulaFromDB) => {
+    res.json(formulaFromDB);
+  })
+  .catch((error) => res.json(error));
+});
+
+// GET /api/formulas - get all formulas associated to the current user
+router.get("/formulas", isAuthenticated, (req, res, next) => {
+  const userId = req.payload._id;
+  
+  Formula.find({ creator: userId })
+    .then((formulasFromDB) => console.log("scurryyyy", formulasFromDB))
+    .catch((error) => res.json(error));
 })
 
 module.exports = router;
