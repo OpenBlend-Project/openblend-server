46483c0b8301dd41c50402e1fcc7777e5cd442d4
binghatch <aron.seiffert@gmail.com>
2023-03-13 15:48:31 +0100

    fix: remove production condition

--- config/index.js
+++ config/index.js
@@ -28,11 +28,4 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-
-  if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
 }
