f4f67920c1901e6a9fecd4f141fcfbb2c962a982
binghatch <aron.seiffert@gmail.com>
2023-03-16 20:43:16 +0100

    fix: remove OPTIONS form allowed methods

--- config/index.js
+++ config/index.js
@@ -13,9 +13,9 @@ module.exports = (app) => {
     if (allowedOrigins.includes(origin)) {
          res.header('Access-Control-Allow-Origin', origin);
     }
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Credentials', false);
     next();
   });
   
